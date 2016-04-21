d30c94e791bd79861072a6958f803df51dbdc577
lakshmajim <lakshmajim>
2016-04-21 18:13:50 +0530

    [ENHANCEMENT] Added sendSMS().
    Added sendSMS() which enables the communiccation in asecure way such that the TWILIO credentials are strored in a env file.
    ==============================================
          AUTHOR : MUTYALA ANANTHA LAKSHMAJI
          DATE   : 21/04/2016
    ==============================================

--- src/Twilio.php
+++ src/Twilio.php
@@ -7,13 +7,11 @@ use Exception;
 require('Services/Twilio.php');
 
 
-
-
 /**
  * Twilio - A Twilio package for sending SMS 
  *
  * @package  Twilio
- * @version  1.0.0
+ * @version  1.0.2
  * @author   lakshmaji 
  */
 
@@ -57,43 +55,65 @@ class Twilio
     private $returnBackUrl;
 
     /**
-    * set sender mobile number.
-    *
-    * @param  string $src
-    * @return response
-    */
+     * set sender mobile number.
+     *
+     * @access public
+     * @param  
+     * @return Response
+     * @since  1.0.2
+     * @author lakshmajim 
+     */
     public function setSourceMobile($src)
     {
         $this->src = $src;
     }
 
+    //-------------------------------------------------------------------------
+
+
     /**
-    * set receiver mobile number.
-    *
-    * @param  string $dest
-    * @return response
-    */
+     * set receiver mobile number.
+     *
+     * @access public
+     * @param  
+     * @return Response
+     * @since  1.0.2
+     * @author lakshmajim 
+     */
     public function setDestinationMobile($dest)
     {
         $this->dest = $dest;
     }
 
+    //-------------------------------------------------------------------------
+
+
     /**
-    * set message that tob sent to destination mobile number.
-    *
-    * @param  string $txt
-    * @return response
-    */
+     * set message that tob sent to destination mobile number.
+     *
+     * @access public
+     * @param  
+     * @return Response
+     * @since  1.0.2
+     * @author lakshmajim 
+     */
     public function setMessageTwilio($txt)
     {
         $this->txt = $txt;
     }
 
+    //-------------------------------------------------------------------------
+
+
     /**
-    * send message using Plivio and Rest api calls.
-    * provide authentication id and authentication token of admin
-    * @param  mixed string $auth_id, $auth_token
-    * @return response
+     * send message using Plivio and Rest api calls.
+     * provide authentication id and authentication token of admin
+     *
+     * @access public
+     * @param  
+     * @return Response
+     * @since  1.0.2
+     * @author lakshmajim 
     */
     public function sendMessageTwilio($auth_id,$auth_token)
     {
@@ -102,19 +122,25 @@ class Twilio
         return $this->sendMessage($this->auth_id,$this->auth_token);
     }
 
+    //-------------------------------------------------------------------------
+
+
     /**
-    * send sms and return the status .
-    *
-    * @param  string $auth_id, $auth_token 
-    * @return response status
-    */
+     * send sms and return the status .
+     *
+     * @access private
+     * @param  
+     * @return Response
+     * @since  1.0.2
+     * @author lakshmajim 
+     */
     private function sendMessage($auth_id,$auth_token)
     {
         // Set message parameters
         $params = array(
-			            'From' => $this->src, 
-			            'To'   => $this->dest, 
-			            'Body' => $this->txt
+                        'From' => $this->src, 
+                        'To'   => $this->dest, 
+                        'Body' => $this->txt
                     );
 
         $client  = new \Services_Twilio($this->auth_id, $this->auth_token); 
@@ -125,9 +151,63 @@ class Twilio
         catch(Exception $smsException)
         {
             return false;
-        }
-                
+        }                 
     }
+
+    //-------------------------------------------------------------------------
+
+
+    /**
+     * send Message to specified mobile number with a constant sender number.
+     *
+     * @access public
+     * @param  
+     * @return Response
+     * @since  1.0.2
+     * @author lakshmajim 
+     */
+    public function sendSMS($userData,$optionalMessage = "You got a message form Ananth ",$sms_type = "SMS") 
+    {
+        // place twilio credentials here
+        $source_mobile = env('TWILIO_SOURCE_NUMBER');
+        $auth_id       = env('TWILIO_AUTH_ID');
+        $auth_secret   = env('TWILIO_AUTH_SECRET');
+        
+        // check sms type
+        if($sms_type == "OTP")
+        {
+            $msgTxt = $optionalMessage." ".$userData['otp'];
+        }
+        else
+        {
+            // sms contains textual information (not OTP)
+            $msgTxt = $optionalMessage;
+        }
+
+        $num_string = $userData['mobile_number'];
+
+        // check whether PLUS sign is appended or not
+        if (strpos($num_string, '+') !== false) 
+        {
+            //PLUS SIGN FOUND
+            $destination_mobile = $num_string;
+
+        }
+        else
+        {
+            $destination_mobile = "+".$num_string;
+        }
+        
+        Twilio::setSourceMobile($source_mobile);
+        Twilio::setDestinationMobile($destination_mobile);
+        Twilio::setMessageTwilio($msgTxt);
+        // authorize and send sms 
+        $smsObject = Twilio::sendMessageTwilio($auth_id,$auth_secret); 
+
+        return $smsObject;
+    }
+
+    //-------------------------------------------------------------------------
 }
 //end of Twilio class
 // end of file Twilio.php
