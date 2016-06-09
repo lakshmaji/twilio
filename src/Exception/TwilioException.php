498adb6dfb9f999f378a16079d83e092429f12c0
lakshmajim <lakshmaji9@gmail.com>
2016-06-09 15:10:11 +0530

    [TASK] Fixed all Bugs

---
+++ src/Exception/TwilioException.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Lakshmajim\Twilio\Exception;
+
+
+/**
+* 
+*/
+class TwilioException extends \Exception
+{
+	protected $satatusCode = 500;
+
+
+    public function __construct($message = 'An error occurred', $statusCode = null)
+    {
+        parent::__construct($message);
+
+        if (! is_null($statusCode)) {
+            $this->setStatusCode($statusCode);
+        }
+    }
+
+    /**
+     * @param int $statusCode
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    /**
+     * @return int the status code
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+}
